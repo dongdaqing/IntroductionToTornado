7a572f94a8aa3c939f056534365ec6d9f7c2ae23
dongdaqing <ddaqing@gmail.com>
2014-08-18 22:16:46 +0800

    charpter4-definitions_readonly

---
+++ charpter4/code_snippets.py
@@ -0,0 +1,8 @@
+__author__ = 'dongdaqing'
+
+import pymongo
+conn = pymongo.Connection("localhost", 27017)
+db = conn.example
+db.words.insert({"word": "oarlock", "definition": "A device attached to a rowboat to hold the oars in place"})
+db.words.insert({"word": "seminomadic", "definition": "Only partially nomadic"})
+db.words.insert({"word": "perturb", "definition": "Bother, unsettle, modify"})
