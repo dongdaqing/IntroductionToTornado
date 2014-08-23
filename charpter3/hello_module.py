7254e29a0eeeac2ef97ca8abbf1f667b015d6c98
dongdaqing <ddaqing@gmail.com>
2014-08-23 10:27:54 +0800

    charpter3-hello_module

---
+++ charpter3/hello_module.py
@@ -0,0 +1,30 @@
+__author__ = 'dongdaqing'
+
+import tornado.web
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import os.path
+
+from tornado.options import define, options
+define("port", default=8888, help="run on the given port", type=int)
+
+class HelloHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('hello.html')
+
+class HelloModule(tornado.web.UIModule):
+    def render(self):
+        return '<h1>Hello,World</h1>'
+
+if __name__ == '__main__':
+    tornado.options.parse_command_line()
+    app = tornado.web.Application(
+        handlers=[(r'/',HelloHandler)],
+        template_path=os.path.join(os.path.dirname(__file__),'templates'),
+        debug=True,
+        ui_modules={'Hello':HelloModule}
+    )
+    server=tornado.httpserver.HTTPServer(app)
+    server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
